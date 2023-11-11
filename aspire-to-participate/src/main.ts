821339aa0c91e94969d2946be036b36659cef7f5
AZH08231 <qoo9986360@gmail.com>
2023-11-11 09:23:13 +0800

    [feat] 新增scss, gsap, iconfy, echart, tailwind設定相關套件

--- aspire-to-participate/src/main.ts
+++ aspire-to-participate/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
+// import "./main.scss";
 import App from "./App.vue";
 import router from "../src/router/index.ts";
 import PrimeVue from "primevue/config";
-
+import { Icon } from '@iconify/vue';
 const app = createApp(App);
+app.component('Icon', Icon)
+
 app.use(router);
 app.use(PrimeVue);
+
 app.mount("#app");
