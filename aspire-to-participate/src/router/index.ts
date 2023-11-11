2a0e87c47337538fc10b9384f6e4c4db351a317a
AZH08231 <qoo9986360@gmail.com>
2023-11-11 09:24:58 +0800

    [fix] 路由調整, 刪除不要的物件, layout, view , 路由環境設定

--- aspire-to-participate/src/router/index.ts
+++ aspire-to-participate/src/router/index.ts
@@ -8,14 +8,38 @@ import {
 // RouterRecordRaw
 const routes: RouteRecordRaw[] = [
   {
-    path: "/demoView",
-    name: "demoView",
-    component: () => import("../../src/components/HelloWorld.vue"),
+    path: '/',
+    component: () => import('@/Layout/DefaultLayout.vue'),
+    children: [
+      {
+        name: 'F2E',
+        path: 'F2E', // 移除斜杠
+        component: () => import('@/views/F2E/F2E.vue'),
+        children: [ // 添加 children 配置
+          {
+            name: 'Site',
+            path: 'Site',
+            component: () => import('@/views/F2E/Site.vue') // 假设有 test1.vue 文件
+          },
+          {
+            name: 'Map',
+            path: 'Map', // 移除斜杠
+            component: () => import('@/views/F2E/Map.vue') // 假设有 test2.vue 文件
+          }
+        ]
+      }
+    ]
   },
   {
-    path: "/:pathMatch(.*)*", // match other path
-    redirect: "/demoView",
+    path:'/login',
+    name:'login',
+    component: ()=> import('@/components/Login.vue')
   },
+  {
+    path: '/:pathMatch(.*)',
+    name: 'Error',
+    component: () => import('../Layout/Error.vue')
+  }
 ];
 
 // RouterOptions
---
+++ aspire-to-participate/src/views/F2E/F2E.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive >
+       <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+<script lang="ts" setup>
+// // const $route = useRoute(); // 导入 useRoute 函数
+// import { onMounted, ref } from 'vue'
+// import { useRouter, useRoute } from 'vue-router'
+// const includeRouterNames = ref(['Site','Map']);
+// const router =  useRoute()
+</script>
