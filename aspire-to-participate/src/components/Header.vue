c43dee8f4a5308ff5aef1638c1ea5265b260f9d3
howard <howard@bitstreetx.com>
2023-11-12 15:34:38 +0800

    wip: edit header

--- aspire-to-participate/src/components/Header.vue
+++ aspire-to-participate/src/components/Header.vue
@@ -1,24 +1,25 @@
 <script lang="ts" setup></script>
 
 <template>
-  <header class="max-h-16">
+  <header class="max-h-16 bg-gray-3">
     <div class="header-wrapper">
       <div class="header-inner flex">
         <div class="logo-wrapper">
-          <div class="logo-inner flex">
-            <div class="logo-english">meow</div>
+          <div class="logo-inner flex text-white">
+            <div class="logo-english ">meow</div>
             <div class="logo-text">喵立翰</div>
           </div>
         </div>
-        <nav class="flex">
-          <dt>候選人主張</dt>
-          <dt>最新活動</dt>
-          <dt>政策議題</dt>
-          <dt>小額捐款</dt>
-          <dt>民眾服務</dt>
+        <nav class="flex text-pink-2">
+          <dt class="mx-11">候選人主張</dt>
+          <dt class="mx-11">最新活動</dt>
+          <dt class="mx-11">政策議題</dt>
+          <dt class="mx-11">小額捐款</dt>
+          <dt class="mx-11">民眾服務</dt>
         </nav>
-        <div class="contact-wrapper">
+        <div class="contact-wrapper bg-pink-2">
           <div class="contact-inner">
+            <div class="contact-select">聯絡我們</div>
             <nav class="contact">
               <dt>Facebook</dt>
               <dt>Instagram</dt>
