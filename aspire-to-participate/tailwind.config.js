1017371ddb87dd8a5dbd4f1efa124884401fab90
AZH08231 <qoo9986360@gmail.com>
2023-11-12 17:41:39 +0800

    [feat] footer 切版RWD完成

--- aspire-to-participate/tailwind.config.js
+++ aspire-to-participate/tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html, js, ts, vue}", 
-    "./src/**/*"
-  ],
+  content: ["./src/**/*.{html, js, ts, vue}", "./src/**/*"],
   corePlugins: {
     preflight: false,
   },
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      'white': '#ffffff',
-      'lightpink':'#EBB2BF',
-      'pink':'#BF6384',
-      'gray':'#D9D9D9',
-      'black':'#2B2B2B'
+      transparent: "transparent",
+      white: "#ffffff",
+      lightpink: "#EBB2BF",
+      pink: "#BF6384",
+      gray: "#D9D9D9",
+      black: "#2B2B2B",
     },
   },
   plugins: [],
