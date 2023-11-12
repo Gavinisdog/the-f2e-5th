9b7b3bb01816c300e854653c51006feffc6d9c59
howard <howard@bitstreetx.com>
2023-11-12 15:17:11 +0800

    chore: style the file & add/rename color in tailwind config

--- aspire-to-participate/tailwind.config.js
+++ aspire-to-participate/tailwind.config.js
@@ -10,12 +10,16 @@ export default {
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      'white': '#ffffff',
-      'lightpink':'#EBB2BF',
-      'pink':'#BF6384',
-      'gray':'#D9D9D9',
-      'black':'#2B2B2B'
+      "transparent": "transparent",
+      "light-1": "#c1c1c1",
+      "light-2": "#b9b9b9",
+      "white": "#fff",
+      "pink-1": "#BF6384",
+      "pink-2": "#EBB2BF",
+      "gray-1": "#D9D9D9",
+      "gray-2": "#434343",
+      "gray-3": "#2B2B2B",
+      "black": "#000",
     },
   },
   plugins: [],
