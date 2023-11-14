51a11b8bad0c46d70d86b33dcd7ba4ec187c5961
howard <howard@bitstreetx.com>
2023-11-14 16:21:55 +0800

    feat: header

--- aspire-to-participate/src/App.vue
+++ aspire-to-participate/src/App.vue
@@ -52,7 +52,6 @@ const countContainerHeight = computed(() => {
 .fade-leave-active {
   transition: opacity 0.5s ease;
 }
-
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
--- aspire-to-participate/src/main.ts
+++ aspire-to-participate/src/main.ts
@@ -4,19 +4,22 @@ import router from "../src/router/index.ts";
 
 import { Icon } from "@iconify/vue";
 import PrimeVue from "primevue/config";
+import Button from "primevue/button";
 import ScrollTop from "primevue/scrolltop";
 import SplitButton from "primevue/splitbutton";
+import Sidebar from "primevue/sidebar";
 
 import "@/main.scss";
 import "primevue/resources/themes/lara-light-purple/theme.css";
 
 const app = createApp(App);
-app.component('Icon', Icon)
 
 app.use(router);
 app.use(PrimeVue);
 
 app.component("Icon", Icon);
+app.component("Button", Button);
 app.component("ScrollTop", ScrollTop);
 app.component("SplitButton", SplitButton);
+app.component("Sidebar", Sidebar);
 app.mount("#app");
