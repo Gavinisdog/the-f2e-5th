529f9841475b6ec0d936e4f671bb52652c96dd81
howard <howard@bitstreetx.com>
2023-11-23 12:35:40 +0800

    wip: github deploy

--- aspire-to-participate/vite.config.ts
+++ aspire-to-participate/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "./",
+  base: "/the-f2e-5th/aspire-to-participate/",
   plugins: [
     vue(),
     createHtmlPlugin({
