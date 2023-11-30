7e396a5add449141a89a059a992e8c15f0723400
howard <howard@bitstreetx.com>
2023-11-30 08:44:21 +0800

    chore: run lint

--- aspire-to-participate/src/router/index.ts
+++ aspire-to-participate/src/router/index.ts
@@ -1,37 +1,37 @@
 import {
-  createRouter,
-  createWebHashHistory,
-  type RouterOptions,
-  type Router,
-  type RouteRecordRaw,
-} from "vue-router";
+	createRouter,
+	createWebHashHistory,
+	type RouterOptions,
+	type Router,
+	type RouteRecordRaw,
+} from 'vue-router';
 // RouterRecordRaw
 const routes: RouteRecordRaw[] = [
-  {
-    path: "/",
-    name: "home",
-    component: async () => import("@/pages/Home/index.vue"),
-  },
-  {
-    path: "/vote",
-    name: "vote",
-    component: async () => import("@/pages/Vote/index.vue"),
-  },
-  {
-    path: "/map",
-    name: "map",
-    component: async () => import("@/pages/Map/index.vue"),
-  },
-  {
-    path: "/:pathMatch(.*)*", // Match other path
-    redirect: "/",
-  },
+	{
+		path: '/',
+		name: 'home',
+		component: async () => import('@/pages/Home/index.vue'),
+	},
+	{
+		path: '/vote',
+		name: 'vote',
+		component: async () => import('@/pages/Vote/index.vue'),
+	},
+	{
+		path: '/map',
+		name: 'map',
+		component: async () => import('@/pages/Map/index.vue'),
+	},
+	{
+		path: '/:pathMatch(.*)*', // Match other path
+		redirect: '/',
+	},
 ];
 
 // RouterOptions
 const options: RouterOptions = {
-  history: createWebHashHistory(),
-  routes,
+	history: createWebHashHistory(),
+	routes,
 };
 
 // Router
